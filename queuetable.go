63bfb63c005d989cbc3d26f1c4aa0422bb978bb8
adskyfly <466703496@qq.com>
2017-07-17 17:29:46 +0800

    modify

--- queuetable.go
+++ queuetable.go
@@ -18,6 +18,9 @@ func (this *QueueTable) Length() int {
 func (this *QueueTable) Pop() interface{} {
 	this.Lock()
 	defer this.Unlock()
+	if len(this.items) <= 0 {
+		return false
+	}
 	value := this.items[0].Data()
 	this.items = this.items[1:]
 	return value
@@ -29,4 +32,14 @@ func (this *QueueTable) Push(item interface{}) bool {
 	data := NewQueueItem(item)
 	this.items = append(this.items, data)
 	return data == this.items[0]
+}
+
+func (this *QueueTable) Pos(n int) interface{} {
+	this.RLock()
+	defer this.RUnlock()
+	if n > len(this.items) || n < 0 {
+		return false
+	}
+
+	return this.items[n].Data()
 }
